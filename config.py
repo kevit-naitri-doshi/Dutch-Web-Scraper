a12f6ec0d012c7c70e29c761cba5fc5ca0b108d1
kevit-naitri-doshi <naitri.doshi@kevit.io>
2024-07-19 15:07:43 +0530

    Final Changes

--- config.py
+++ config.py
@@ -3,5 +3,21 @@ main_menu_urls={'At Home':'','For Companies':'zakelijk'}
 service_page_selector=['homepage','homepagezakelijk']
 core_menu=[]    
 main_menu_index_last=[4,5]
-    
 service_page_index=[4,3]
+
+extra_headers = {
+    'Sec-Ch-Ua':
+    '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    'Sec-Ch-Ua-Mobile':
+    '?0',
+    'Sec-Ch-Ua-Platform':
+    "Linux",
+    'Sec-Fetch-Dest':
+    'empty',
+    'Sec-Fetch-Mode':
+    'cors',
+    'Sec-Fetch-Site':
+    'same-site',
+    }
+
+user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
